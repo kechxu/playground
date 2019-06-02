3da8b7e6e025974632c468d2034f24b97549909b
kechxu <kechxu89@gmail.com>
2019-06-02 14:41:59 -0700

    Add framework of is_permutation.py

---
+++ python_practice/is_permutation.py
@@ -0,0 +1,13 @@
+# Is a string is the permutation of another string
+
+def isPermutation(str1, str2):
+    """
+    @brief To determine if str1 is the permutation of str2
+    @param str1: a string
+    @param str2: a string
+    @return True or False, if str1 is the permutation of str2
+    """
+    # Add your code here
+
+    return True
+
