c125da6a425d45c90c43ed758a9791575ded3aa7
kechxu <kechxu89@gmail.com>
2019-06-02 21:16:32 -0700

    Add framework of vector_sum

---
+++ python_practice/vector_sum.py
@@ -0,0 +1,21 @@
+# Compute the sum of a vector
+
+def vector_sum(nums):
+    """
+    @brief: compute the sum of a vector, (or list in python)
+    @param num: the vector
+    @return the sum of elements nums
+    """
+    # Add code here
+
+    return 0.0
+
+
+if __name__ == "__main__":
+
+    nums1 = [1, 2, 3]
+    if (vector_sum(nums1) == 6):
+        print("Test Success!")
+    else:
+        print("Test Failed!")
+
